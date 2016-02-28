ca5259defbff98982c23eeb5b14b72883e073a58
Aidan Zebertavage <aidanzeber@gmail.com>
2016-02-28 23:00:22 +0000

    built blur method

--- image.rb
+++ image.rb
@@ -1,10 +1,6 @@
 class Image 
-	def initialize
-		@image = [
-	[0,0,0,0], 
-	[0,1,0,0], 
-	[0,0,0,1],
-	[0,0,0,0]]
+	def initialize(image)
+		@image = image
 	end
 
 	def output_image
@@ -15,7 +11,20 @@ class Image
 			print "\n"
 		end
 	end
+
+	def blur 
+		@image.each_with_index do |row, row_index|
+			row.each_with_index do |column, column_index|
+		end
+	end
+
+
 end
 
-image = Image.new
+image = Image.new([
+	[0,0,0,0], 
+	[0,1,0,0], 
+	[0,0,0,1],
+	[0,0,0,0]])
 image.output_image
+image.blur
