805431d83bc3a061b16558442d7bb3c698a4f1a6
Aidan Zebertavage <aidanzeber@gmail.com>
2016-03-09 00:08:07 +0000

    completed imageblur 2

--- imageblur2.rb
+++ imageblur2.rb
@@ -3,23 +3,41 @@ class Image
     @image = image
   end
 
-  def output_image
-    @image.each do |arr|
-      arr.each do |item|
-        print "#{item}"
-      end
-      print "\n"
-    end
-  end
-
-  def blur 
+  def check 
+    ones = []
     @image.each_with_index do |row, row_index|
-      row.each_with_index do |column, column_index|
+      row.each_with_index do |pixel, column_index|
+        if pixel == 1
+          ones.push([row_index, column_index])
+        end
+      end
     end
+    ones.each do |row_index, column_index|
+      blur(row_index, column_index)
+    end
+
+    return Image.new(@image)
   end
 
+  def blur(row_index, column_index)
+    update_cell(row_index+1, column_index,1)
+    update_cell(row_index, column_index+1, 1)
+    update_cell(row_index, column_index-1, 1)
+    update_cell(row_index-1, column_index, 1)
+  end
 
-end
+  def update_cell (row_index,column_index,value)
+    return if !within?(@image, row_index) || !within?(@image[row_index], column_index)
+    @image[row_index][column_index] = value
+  end
+
+  def within?(array, index)
+    array.size > index && index >=0
+  end
+
+  def output_image
+    @image.each { |row| puts row.join}
+  end
 end
 
 image = Image.new([
@@ -27,5 +45,5 @@ image = Image.new([
   [0,1,0,0], 
   [0,0,0,1],
   [0,0,0,0]])
-image.output_image
-image.blur
+blurImage = image.check
+blurImage.output_image
