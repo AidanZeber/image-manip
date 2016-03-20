7a5eac3b5d168329af6580b35834621db02b20da
Aidan Zebertavage <aidanzeber@gmail.com>
2016-03-20 16:33:39 +0000

    working on imageblur 3, error exists with blur method

--- imageblur3.rb
+++ imageblur3.rb
@@ -15,22 +15,24 @@ class Image
         end
       end
     end
-    #Now that you've created the one_index area, it contains the coordinates of where the ones existed in the original image, 
-    #iterate through this array of pixel locations and pass those values into the method to blur. 
-    one_index.each do |row_index, col_index|
-      blur(row_index,col_index)
-    end
-
-    return Image.new(@image)
+    return one_index
   end
-
   #blur method takes the row index and column index as arguments. These are the locations of where the "ones" pixels existed in the original image. 
   #the update cell method takes these arguments and the update to the surrounding cells based on the +1, -1. This causes the image to "blur". 
-  def blur(row_index,col_index)
+  def transform(one_index)
+    one_index.each do |row_index, col_index|
     update_cell(row_index+1, col_index,1)
     update_cell(row_index,col_index+1, 1)
     update_cell(row_index,col_index-1, 1)
     update_cell(row_index-1, col_index,1)
+  end
+  return Image.new(@image)
+end
+
+  def blur(n)
+    n.times do 
+      transform(one_index)
+    end
   end
 
 #update cell method changes the values passed into it into a 1 but we need to check to make sure that we are within the bounds of the original image. 
@@ -56,4 +58,8 @@ image = Image.new([
 ])
 
 checkImage = image.check
+#checkImage.blur(2)
 checkImage.output_image
+
+
+
